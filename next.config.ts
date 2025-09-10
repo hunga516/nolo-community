becd3270c0d58702722d801dbfb49e426115036a
Dev NgocLoc <53590463+hunga516@users.noreply.github.com>
2025-09-11 03:01:23 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,70 +4,74 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'image.mux.com',
-        port: '',
-        pathname: '/**',
-        search: '',
+        protocol: "https",
+        hostname: "image.mux.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'utfs.io',
-        port: '',
-        pathname: '/**',
-        search: '',
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'img.clerk.com',
-        port: '',
-        pathname: '/**',
-        search: '',
+        protocol: "https",
+        hostname: "img.clerk.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**',
-        search: '',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'img.vietqr.io',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "img.vietqr.io",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'ik.imagekit.io',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'upload.wikimedia.org',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'pngimg.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "pngimg.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'i.pinimg.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "i.pinimg.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: '1000marcas.net',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "1000marcas.net",
+        pathname: "/**",
       },
     ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // áp dụng cho tất cả route
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL", // hoặc "SAMEORIGIN" nếu chỉ muốn nhúng trong cùng domain
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *", // cho phép mọi trang nhúng
+          },
+        ],
+      },
+    ];
   },
 };
 
